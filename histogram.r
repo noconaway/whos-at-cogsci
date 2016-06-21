7cb172b21e051e2ff91569450659ad7a8512c547
Nolan <nconawa1@binghamton.edu>
2016-06-21 00:08:20 -0400

    frequency chart working

--- histogram.r
+++ histogram.r
@@ -2,56 +2,45 @@ if(dev.cur() != 1) {dev.off()}
 rm(list=ls())
 options(width=100)
 
-
-
 # load data
 master <- read.table('data.tsv', quote = "", sep="\t", 
 	row.names = 1, header=TRUE)
-
 numpresentations = rowSums(master)
-names = row.names(master)
 
-
-# get hover info
-hovertxt = c('','','','','','','','','','')
-for (i in 1:10) {
-	N = sum(numpresentations==i)
-	if (N<20 & N>0) { 
-		hovertxt[i] = paste(names[numpresentations==i],collapse='<br>')
-	}
+# get list of last names for sorting
+lastnames = vector(mode="character", length = length(numpresentations))
+for (i in 1:length(numpresentations)) {
+	name = row.names(master)[i]
+	lastnames[i] = substr(name,start = 2, stop = nchar(name))
 }
+sortorder = sort(lastnames, index.return = TRUE)$ix
 
 
+# sosrt data fields
+fullnames = row.names(master)[sortorder]
+numpresentations = numpresentations[sortorder]
+index = 1:length(numpresentations)
+
+require(rCharts)
+
+# data
+df <- data.frame(x = index, y = numpresentations, z = fullnames)
 
 
+# create plot object
+p <- hPlot(y ~ x, data = df, type = "scatter")
 
+# set turbothreshold
+p$plotOptions(series=list(turboThreshold = 2000))
 
-library(plotly)
+# fix formatting
+p$params$series[[1]]$data <- toJSONArray(df, json = F)
 
-font <- list(
-  family = "Courier New, monospace",
-  size = 18,
-  color = "#000000"
-)
+# set tooltip
+p$tooltip(formatter = "#! function() {return(this.point.z);} !#")
 
-xlab <- list(
-  title = "Number of Presentations",
-  titlefont = font,
-  tickfont = font,
-  dtick = 1
-)
-ylab <- list(
-  title = "#",
-  titlefont = font,
-  tickfont = font
-)
+# axis formatting
+p$yAxis(title = list(text = "Number of Presentations"), categories = 0:10)
+p$xAxis(title = list(text = "Author") )
 
-
-p = plot_ly(x = numpresentations, type="histogram", 
-	nbinsx = 10, nticks = 10,
-	borderwidth = 2, opacity = 0.9, 
-	hoverinfo = 'text', text = hovertxt) %>%
-	layout(xaxis = xlab, yaxis = ylab)
-print(p)
-# htmlwidgets::saveWidget(as.widget(p), "index.html")
-
+p$save('plots/frequency.html', cdn = TRUE)
